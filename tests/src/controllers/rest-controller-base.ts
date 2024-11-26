b1f1ca422249d60474779bf5db23b42b70a9072d
Matheus Icaro <matheusicaro2@hotmail.com>
2024-11-26 00:48:51 -0700

    Added RestControllerBase

---
+++ tests/src/controllers/rest-controller-base.ts
@@ -0,0 +1,19 @@
+/**
+ * TODO: implement tests for RestControllerBase
+ *     - issue: https://github.com/matheusicaro/mi-node-framework/issues/13
+ */
+describe('RestControllerBase', () => {
+  describe('constructor', () => {
+    test('should set the default fields correctly', () => {});
+
+    test('should set custom fields from the args correctly when they are informed', () => {});
+  });
+
+  describe('responseFailRequestWithErrorHandle', () => {
+    test('should log errors correctly', () => {});
+
+    test('should response with the correct status code and payload', () => {});
+
+    test('should response with payload incremented with responseData fields', () => {});
+  });
+});
