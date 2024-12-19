6e9f2fa6cb7455fb9e510db82d508ac627c4fdb3
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-19 10:03:54 -0700

    exporting testing

--- src/testing/auto-stub/index.ts
+++ src/testing/auto-stub/index.ts
@@ -1,10 +1,14 @@
 import { jestStub } from './jest-auto-stub';
 import { vitestStub, VitestStub } from './vitest-auto-stub';
 
+/**
+ * Stub interfaces, types and objects for Jest or Vitest
+ * How to use it: =====>TODO
+ */
 const stub = {
   jest: jestStub,
   vitest: vitestStub
 };
 
-export * from './deep-stub';
 export { stub, VitestStub };
+export * from './deep-stub';
