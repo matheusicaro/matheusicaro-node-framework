65807f69da5c4543090aec8ec4c3b5bb2187dbc3
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-19 11:52:51 -0700

    Update src/testing/factory/factory.ts
    Signed-off-by: Matheus Icaro <matheusicaro2@hotmail.com>

--- src/testing/factory/factory.ts
+++ src/testing/factory/factory.ts
@@ -5,7 +5,7 @@ import { DeepPartial, Factory as FisheryFactory, GeneratorFnOptions, GeneratorFn
  * Factory will allow you to create objects easier.
  *
  * how to use?
- * ref: TODO
+ * ref: https://github.com/matheusicaro/matheusicaro-node-framework/blob/28-add-factory-builder-for-unit-tests/README.md#factory
  */
 class Factory<T, I = unknown> extends FisheryFactory<T, I> {
   constructor(generatorFunction: (opts: GeneratorFnOptions<T, I, any>) => T) {
