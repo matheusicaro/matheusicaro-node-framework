e808097ddecd2dd87fc769a66ece55d93942a851
Matheus Icaro <matheusicaro2@hotmail.com>
2026-09-14 15:43:01 -0700

    feat: add sleep utility function and update utils index export

---
+++ src/utils/sleep.ts
@@ -0,0 +1,17 @@
+import { promisify } from 'util';
+
+/**
+ * Pause the thread, useful to avoid throughput issues when processing large amounts of data.
+ *
+ * How to use: https://github.com/matheusicaro/matheusicaro-node-framework/tree/master?tab=readme-ov-file#sleep
+ *
+ * @param delay: in milliseconds
+ *
+ * @example
+ * ```
+ *  await sleep(1000) // sleep for 1 second
+ * ```
+ */
+const sleep = promisify(setTimeout);
+
+export { sleep };
